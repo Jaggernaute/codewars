3b51a34871c4fef8af44198bc863a5abf72d8483
Jaggernaute <lucas.videcoq1@gmail.com>
2022-03-24 10:52:32 +0100

    kata Volume of a cuboid [8kyu]

--- CW-Volume-of-a-Cuboid/main.cpp
+++ CW-Volume-of-a-Cuboid/main.cpp
@@ -1,10 +1,28 @@
 //
 // Created by jaggernaute on 3/23/22.
+// 8kyu
 //
 
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "==========================================================" << std::endl;
+    std::cout << "|                                                        |" << std::endl;
+    std::cout << "|                   volume of a cuboid                   |" << std::endl;
+    std::cout << "|                                                        |" << std::endl;
+    std::cout << "==========================================================" << std::endl;
+
+    std::cout << "enter the length, width, and height of cuboid: " << std::endl;
+    double length, width, height;
+    std::cout << "length: ";
+    std::cin >> length;
+
+    std::cout << "width: ";
+    std::cin >> width;
+
+    std::cout << "height: ";
+    std::cin >> height;
+
+    std::cout << length * width * height << std::endl;
     return 0;
 }
